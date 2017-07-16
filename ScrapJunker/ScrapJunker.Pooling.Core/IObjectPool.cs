ac23391164c9688e4043b937134725f42922dd83
DESKTOP-TUN3N6H\Vlad Apetre <vlad.apetre@yahoo.com>
2017-07-16 18:08:59 +0300

    no message

---
+++ ScrapJunker/ScrapJunker.Pooling.Core/IObjectPool.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ScrapJunker.Pooling.Core.Interface
+{
+    public interface IObjectPool<TObject>
+    {
+        TObject GetObject();
+        void PutObject(TObject item);
+    }
+}
---
+++ ScrapJunker/ScrapJunker.Pooling/Base/ObjectPool.cs
@@ -0,0 +1,36 @@
+﻿using ScrapJunker.Pooling.Core;
+using ScrapJunker.Pooling.Core.Interface;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapJunker.Pooling.Base
+{
+    public class ObjectPool<TObject> : IObjectPool<TObject>
+    {
+        private ConcurrentBag<TObject> _objects;
+        private Func<TObject>_objectGenerator;
+
+        public ObjectPool(Func<TObject> objectGenerator)
+        {
+            if (objectGenerator == null) throw new ArgumentNullException("objectGenerator");
+            _objects = new ConcurrentBag<TObject>();
+            _objectGenerator = objectGenerator;
+        }
+
+        public TObject GetObject()
+        {
+            TObject item;
+            if (_objects.TryTake(out item)) return item;
+            return _objectGenerator();
+        }
+
+        public void PutObject(TObject item)
+        {
+            _objects.Add(item);
+        }
+    }
+}
