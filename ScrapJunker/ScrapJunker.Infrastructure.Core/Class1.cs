1e1a2cb771b8819e251f6b58c0c31a47db058a6a
DESKTOP-TUN3N6H\Vlad Apetre <vlad.apetre@yahoo.com>
2017-07-16 13:21:37 +0300

    basic projects

---
+++ ScrapJunker/ScrapJunker.Infrastructure.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapJunker.Infrastructure.Core
+{
+    public class Class1
+    {
+    }
+}
