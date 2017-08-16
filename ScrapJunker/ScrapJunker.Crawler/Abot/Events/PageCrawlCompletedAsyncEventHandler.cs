89c9dbc83b1058115c562d234c8c98967128146c
vladapetre <vlad.apetre@yahoo.com>
2017-08-16 17:06:10 +0300

    strp Style, Link, Script from crawled pages , html changes

--- ScrapJunker/ScrapJunker.Crawler/Abot/Events/PageCrawlCompletedAsyncEventHandler.cs
+++ ScrapJunker/ScrapJunker.Crawler/Abot/Events/PageCrawlCompletedAsyncEventHandler.cs
@@ -1,7 +1,9 @@
 ﻿using Abot.Crawler;
+using HtmlAgilityPack;
 using ScrapJunker.Infrastructure.Base;
 using ScrapJunker.Infrastructure.Core.Interface;
 using System.IO;
+using System.Linq;
 using System.Net;
 
 namespace ScrapJunker.Crawler.Abot.Events
@@ -55,8 +57,15 @@ namespace ScrapJunker.Crawler.Abot.Events
             }
             else
             {
-                var path = Path.Combine(Directory.GetCurrentDirectory(), "Storage", "Abot");
-                _storage.StoreCrawledPage(e.CrawledPage.Content.Text, e.CrawledPage.Uri, "test token");
+               
+                var doc = new HtmlDocument();
+                doc.LoadHtml(e.CrawledPage.Content.Text);
+
+                var nodes = doc.DocumentNode.SelectNodes("//script|//style|//link");
+
+                foreach (var node in nodes)
+                    node.ParentNode.RemoveChild(node);
+                _storage.StoreCrawledPage(doc.DocumentNode.OuterHtml, e.CrawledPage.Uri, "test token");
             }
         }
     }
