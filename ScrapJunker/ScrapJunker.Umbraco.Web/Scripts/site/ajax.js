6da430dfa090b83366bf6744eebfef2ee2013ef2
vladapetre <vlad.apetre@yahoo.com>
2017-08-17 10:48:04 +0300

    exctracted js and css into files

---
+++ ScrapJunker/ScrapJunker.Umbraco.Web/Scripts/site/ajax.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    $("form")
+        //.formValidation()
+        .on('submit', function (e) {
+            // Prevent form submission
+            e.preventDefault();
+
+            var $form = $(e.target),
+                fv = $form.data('formValidation');
+
+            // Use Ajax to submit form data
+            $.ajax({
+                url: $form.attr('action'),
+                type: 'POST',
+                data: $form.serialize(),
+                beforeSend: function () {
+                    debugger;
+                    $form.LoadingOverlay("show");
+                },
+                success: function (result) {
+                    toastr.success("Success!");
+                },
+                error: function (result) {
+                    if (result.responseJSON.ModelState !== undefined) {
+                        var errors = [];
+                        for (var key in result.responseJSON.ModelState) {
+                            for (var i = 0; i < result.responseJSON.ModelState[key].length; i++) {
+                                errors.push(result.responseJSON.ModelState[key][i]);
+                            }
+                        }
+                        toastr.error(errors);
+                    }
+                    else if (result.responseJSON.ExceptionMessage !== undefined) {
+                        toastr.error(result.responseJSON.ExceptionMessage);
+                    } else {
+                        toastr.error(result.responseJSON.Message);
+                    }
+                },
+                complete: function () {
+                    $form.LoadingOverlay("hide");
+                }
+            });
+        });
+});
---
+++ ScrapJunker/ScrapJunker.Umbraco.Web/Scripts/site/master.js
@@ -0,0 +1,32 @@
+﻿function verticalAlignMiddle() {
+    var bodyHeight = $(window).height();
+    var formHeight = $('.vamiddle').height();
+    var marginTop = (bodyHeight / 4) - (formHeight / 4);
+    if (marginTop > 0) {
+        $('.vamiddle').css('margin-top', marginTop);
+    }
+}
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $('#backToTop').fadeIn();
+    } else {
+        $('#backToTop').fadeOut();
+    }
+}
+
+$('#backToTop').click(function () {
+    $('body,html').animate({
+        scrollTop: 0
+    }, 800);
+    return false;
+});
+
+
+$(document).ready(function () {
+    verticalAlignMiddle();
+});
+
+$(window).bind('resize', verticalAlignMiddle);
